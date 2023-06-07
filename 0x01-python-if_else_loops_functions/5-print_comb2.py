55ab11c2fbb58efd36bbccc6b05358f245e56828
fr13ndx <morsewallace254@gmail.com>
2023-06-06 19:54:04 -0700

    004

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
     if n == 99:
-        print("{}".format(number))
+        print("{}".format(n))
     else:
         print("{:02}".format(n), end=", ")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-	#65 - 90
-	for n in str:
-		if ord(n) >= 97 and ord(n) <= 122:		
-			n = chr(ord(n) - 32)
-		print(f"{n}", end="")
-	print("")
+    for n in str:
+        if ord(n) >= 97 and ord(n) <= 122:
+            n = chr(ord(n) - 32)
+        print("{}".format(n), end="")
+    print("")
